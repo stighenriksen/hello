b0451527b846cb3387c3a71066d922312e7e7b87
Stig Henriksen <mail@stighenriksen.com>
2014-09-14 18:38:45 +0200

    Add release.nix

---
+++ release.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> {}; 
+
+  jobs = rec { 
+
+    tarball = 
+      pkgs.releaseTools.sourceTarball { 
+        name = "hello-tarball";
+        src = <hello>; 
+        buildInputs = (with pkgs; [ gettext texinfo ]);
+      };
+
+    build = 
+      { system ? builtins.currentSystem }:  
+
+      let pkgs = import <nixpkgs> { inherit system; }; in
+      pkgs.releaseTools.nixBuild { 
+        name = "hello";
+        src = jobs.tarball;
+        configureFlags = [ "--disable-silent-rules" ];
+      };
+  };
+in
+  jobs 
